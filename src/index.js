ee4fb340bdd879604862f65caef2a1fa0dab8fbd
Dico Diaz <dicodiaz@gmail.com>
2021-09-22 14:22:39 -0500

    Add important functionality
    * Add interactive check/uncheck function
    * Add local storage usage to save/load user checked tasks

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import Enter from './enter-arrow.png';
 new Task({ description: 'Wash the dishes', completed: false, index: 1 }).add();
 new Task({ description: 'Complete To Do List project', completed: false, index: 2 }).add();
 new Task({ description: 'Attend standup meeting', completed: false, index: 3 }).add();
+new Task({ description: 'Go to a barbershop', completed: false, index: 3 }).add();
 const toDoList = document.querySelector('#to-do-list');
 const refreshContainer = document.querySelector('#refresh');
 const enterContainer = document.querySelector('#enter');
 
 window.onload = () => {
+  Task.loadLocalTasks();
   Task.populate(toDoList);
   appendImg(Refresh, refreshContainer, false);
   appendImg(Enter, enterContainer, false);
