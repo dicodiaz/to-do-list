6109af456c703e455a97efa06cdbe31b9741e054
Dico Diaz <dicodiaz@gmail.com>
2021-09-23 22:48:18 -0500

    Add drag and drop functionality

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { appendImg } from './images.js';
 import Refresh from './refresh.png';
 import Enter from './enter-arrow.png';
 
-const toDoList = document.querySelector('#to-do-list');
+const container = document.querySelector('#to-do-list');
 const refreshContainer = document.querySelector('#refresh');
 const enterContainer = document.querySelector('#enter');
 const addTask = document.querySelector('#add-task');
@@ -12,16 +12,34 @@ const enter = document.querySelector('#enter');
 const warning = document.querySelector('#warning');
 const clearAll = document.querySelector('#clear-all');
 
+const dragoverHandler = (e) => {
+  e.preventDefault();
+  const y = e.clientY || e.touches[0].clientY;
+  const afterElement = Task.getDragAfterElement(container, y);
+  const draggable = document.querySelector('.dragging');
+  const draggableIndex = Task.tasks.findIndex((task) => task.node === draggable);
+  let afterElementIndex;
+  if (!afterElement) {
+    container.appendChild(draggable);
+    afterElementIndex = Task.tasks.length - 1;
+  } else {
+    container.insertBefore(draggable, afterElement);
+    afterElementIndex = Task.tasks.findIndex((task) => task.node === afterElement) - 1;
+  }
+  if (draggableIndex > afterElementIndex) afterElementIndex++;
+  Task.tasks[draggableIndex].afterElementIndex = afterElementIndex;
+};
+
 window.onload = () => {
   Task.loadLocalTasks();
-  Task.populateAll(toDoList);
+  Task.populateAll(container);
   appendImg(Refresh, refreshContainer);
   appendImg(Enter, enterContainer);
 
   enter.addEventListener('click', () => {
     if (addTask.value) {
       const task = new Task({ description: addTask.value });
-      task.add().populate(toDoList, task.index - 1);
+      task.add().populate(container, task.index - 1);
       addTask.value = '';
     } else {
       warning.classList.remove('d-none');
@@ -37,5 +55,13 @@ window.onload = () => {
 
   clearAll.addEventListener('click', () => {
     Task.clearAllCompleted();
+  });
+
+  container.addEventListener('dragover', (e) => {
+    dragoverHandler(e);
+  });
+
+  container.addEventListener('touchmove', (e) => {
+    dragoverHandler(e);
   });
 };
