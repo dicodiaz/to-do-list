0e266cacb6f76decf20e06c312a89963c6653a73
Dico Diaz <dicodiaz@gmail.com>
2021-09-29 16:20:11 -0500

    Add tests for removing functionality

--- src/__tests__/add-remove.test.js
+++ src/__tests__/add-remove.test.js
@@ -5,25 +5,21 @@
 import TaskManager from '../TaskManager.js';
 import LocalStorage from '../__mocks__/LocalStorage.js';
 
-
-
-
 global.localStorage = new LocalStorage();
 
-// 
+const task = { description: 'asdf', completed: false, index: 1 };
+const task2 = { description: 'asdf2', completed: false, index: 2 };
+const taskManager = new TaskManager();
+document.body.innerHTML = `<table><tbody id="to-do-list"></tbody></table>`;
+const container = document.getElementById('to-do-list');
 
-describe('Adding and removing items', () => {
-  const task = { description: 'asdf', completed: false, index: 1 };
-  const task2 = { description: 'asdf2', completed: false, index: 2 };
+describe('Testing add items in the database and in local Storage', () => {
   test('Adding an item', () => {
-    const taskManager = new TaskManager();
     taskManager.add(task);
-    expect(taskManager.tasks.length).toBeGreaterThan(0);
+    expect(taskManager.tasks.length).toBe(1);
   });
 
   test('Adding two items', () => {
-    const taskManager = new TaskManager();
-    taskManager.add(task);
     taskManager.add(task2);
     expect(taskManager.tasks.length).toBe(2);
   });
@@ -34,27 +30,31 @@ describe('Adding and removing items', () => {
   });
 });
 
+describe('Testing DOM Manipulation', () => {
+  test('Checking DOM when adding an item', () => {
+    taskManager.populate(container, task);
+    const trs = container.querySelectorAll('tr');
+    expect(trs).toHaveLength(1);
+  });
 
-describe("Testing DOM Manipulation", () => {
-  const task = { description: 'asdf', completed: false, index: 1 };
-  const task2 = { description: 'Test', completed: false, index: 2 };
-  document.body.innerHTML = `<table><tbody id="to-do-list"></tbody></table>`;
-  const taskManager = new TaskManager();
-  const container = document.getElementById("to-do-list")
+  test('Checking DOM when adding two elements', () => {
+    taskManager.populate(container, task2);
+    expect(container.querySelectorAll('tr')).toHaveLength(2);
+  });
 
-  test("Checking DOM when adding an item", () => {
-    taskManager.populate(container, task)
-    const trs = container.querySelectorAll("tr")
-    expect(trs).toHaveLength(1)
-  })
+  test('Deleting an element from DOM', () => {
+    taskManager.remove(task);
+    expect(container.querySelectorAll('tr')).toHaveLength(1);
+  });
+});
 
-  test("Checking DOM when adding two elements", () => {
-    taskManager.populate(container, task2)
-    expect(container.querySelectorAll("tr")).toHaveLength(2)
-  })
-
-  test("Deleting an element in DOM", () => {
-    taskManager.remove(task)
-    expect(container.querySelectorAll("tr")).toHaveLength(1)
-  })
-})
+describe('Testing remove in database and in local storage', () => {
+  test('Adding and removing an element in tasks array and in local storage', () => {
+    taskManager.add(task);
+    expect(taskManager.tasks.length).toBe(2);
+    expect(JSON.parse(localStorage.getItem('tasks')).length).toBe(2);
+    taskManager.remove(task);
+    expect(taskManager.tasks.length).toBe(1);
+    expect(JSON.parse(localStorage.getItem('tasks')).length).toBe(1);
+  });
+});
